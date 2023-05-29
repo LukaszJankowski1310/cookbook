fa8d11a5645f6da6bf2393470ebedc5a69cefff8
Łukasz <lukasz13102001@o2.pl>
2023-05-29 21:17:58 +0200

    Recycler view done

--- app/src/main/java/com/example/cookbook/DishListFragment.kt
+++ app/src/main/java/com/example/cookbook/DishListFragment.kt
@@ -2,29 +2,28 @@ package com.example.cookbook
 
 import android.content.Context
 import android.os.Bundle
-import android.util.Log
 
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ListView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.ListFragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cookbook.api.model.Dish
+import com.example.cookbook.tmp.MyAdapter
 
 
-class DishListFragment : ListFragment() {
+class DishListFragment : Fragment() {
 
     private lateinit var listener: Listener
     private val viewModel: DishesViewModel by lazy {
         (requireActivity().application as App).myViewModel
     }
 
-    private val titles : ArrayList<String> = ArrayList()
+//    private val titles : ArrayList<String> = ArrayList()
     private val dishes : ArrayList<Dish> = ArrayList()
 
     private lateinit var recyclerView: RecyclerView
@@ -43,33 +42,34 @@ class DishListFragment : ListFragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return super.onCreateView(inflater, container, savedInstanceState)
+
+        val view = inflater.inflate(R.layout.fragment_dish_list, container, false)
+        recyclerView = view.findViewById(R.id.dish_list_rv)
+        recyclerView.layoutManager = LinearLayoutManager(activity)
+
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+//        val adapter = MyAdapter(dishes) { item ->
+//            listener.itemClicked(item)
+//        }
+//        recyclerView.adapter = adapter
 
-
-
-        val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, titles)
-        listAdapter = adapter
         viewModel.getData().observe(viewLifecycleOwner, Observer { newData ->
-            Log.i("Observer", "DishListFragment")
-            titles.clear()
+            dishes.clear()
             newData.results.forEach { item ->
                 run {
-                    titles.add(item.title)
+                    dishes.add(Dish(item.title, item.image))
                 }
             }
-            val adapter = ArrayAdapter<String>(requireContext(), android.R.layout.simple_list_item_1, titles)
-            listAdapter = adapter
 
+            val adapter = MyAdapter(dishes) { item ->
+                listener.itemClicked(item)
+            }
+            recyclerView.adapter = adapter
         })
-    }
-
-    override fun onListItemClick(l: ListView, v: View, position: Int, id: Long) {
-        super.onListItemClick(l, v, position, id)
-        listener.itemClicked(id)
     }
 
 
--- app/src/main/java/com/example/cookbook/tmp/DishListRecyclerView.kt
+++ app/src/main/java/com/example/cookbook/tmp/DishListRecyclerView.kt
@@ -8,16 +8,18 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.cookbook.R
 import com.example.cookbook.api.model.Dish
 
 
 class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-    val text: TextView = view.findViewById(R.id.title_text_view)
+    val title: TextView = view.findViewById(R.id.title_text_view)
     val image : ImageView = view.findViewById(R.id.image_view)
+
 }
 
-class MyAdapter(private val itemList: List<Dish>) :
+class MyAdapter(private val itemList: List<Dish>, private val onItemClick: (Long) -> Unit) :
     RecyclerView.Adapter<MyViewHolder>() {
 
 
@@ -31,8 +33,15 @@ class MyAdapter(private val itemList: List<Dish>) :
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
         val item = itemList[position]
-        Log.i("item", item.toString())
-        holder.text.text = item.toString()
+
+        holder.itemView.setOnClickListener {
+            onItemClick(position.toLong())
+        }
+
+        holder.title.text = item.title
+        Glide.with(holder.itemView)
+            .load(item.image)
+            .into(holder.image)
     }
 
     override fun getItemCount(): Int {
